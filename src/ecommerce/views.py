18513cac3ad138b0f924ad13aa5ae5d6a33bbad5
“Lakmal <lakmal@huubap.com>
2020-06-12 04:17:37 +0800

    Added initial home page

---
+++ src/ecommerce/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_page(request):
+	return HttpResponse("Python_Ecommerce_Homepage_Lakmal_test")
+
+def about_page(request):
+    return HttpResponse("Python_Ecommerce_Homepage_Lakmal_test")
+
+def contact_page(request):
+    return HttpResponse("Python_Ecommerce_Homepage_Lakmal_test")
+
