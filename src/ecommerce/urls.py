94bfa1be24fe4b4f52b077e086ea21e294434eed
“Lakmal <lakmal@huubap.com>
2020-07-25 16:07:14 +0800

    Added register forms

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -5,7 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path(''  , views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -16,8 +16,10 @@ Including another URLconf
 #from django.contrib import admin
 from django.urls import path
 from . import views
+from.views import home_page,about_page, contact_page, login_page
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('about/', views.about_page, name='about_page'),
     path('contact/', views.contact_page, name='contact_page'),
+    path('login/', views.login_page, name ='login'),
 ]
